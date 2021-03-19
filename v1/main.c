74ff0cea0d75628b2162820c680faebe0b52da36
Jiwoo <77063672+jweun1@users.noreply.github.com>
2021-03-19 23:03:33 +0900

    Update main.c

--- v1/main.c
+++ v1/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]){
 	char* absDir;
 	
 	if(argc != 3){ //Need more data
-		fprintf(stderr, "Usage : Program [-Dir] [-File]\n");
+		fprintf(stderr, "Usage : Program [-Dir] [-Dir/-File]\n");
 		exit(1);
 	}
 
@@ -15,25 +15,31 @@ int main(int argc, char *argv[]){
 	scanf("%d",&BD);
 
 	if((BD==0)||(BD==1)){
-		//준호
-		absDir = absolute(argv[1]);
-		dfs_or_bfs(absDir,BD);
-		printf("juno3\n");
+		
+		if (BD==0)
+			printf("BFS Search\n");
+		else
+			printf("DFS Search\n");
+		printf("\n");
 
-		//지우
+		//path
 		char tmp_Path[MAX];
 		strcpy(tmp_Path,argv[2]);
 		char* toFind = extract_Filename(tmp_Path); //입력받은 argv[2]에서 파일 또는 디렉터리 이름만을 추출해 toFind에 넣음
 		before_Search(argv[2], toFind, BD);
+		printf("\n");
 
+		//size && tree
+		init(argv[1]);
+		absDir = absolute(argv[1]); //size
+		bfs_or_dfs(absDir,BD);
+		printf("\n");
+		
+		printf("Tree:"); //tree
+		selectmod(argv[1], BD);
 	}
 	else
 		printf("Select Error(BFS:0, DFS:1)\n");
-	
-	//익준
-	printf("ikjun");//
-	init(argv[1]); 
-	selectmod(argv[1]);
 
 	return 0;
 }
