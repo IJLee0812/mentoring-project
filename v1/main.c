4c8a610c045de3a688770559deb565fa32ab396c
Jiwoo <77063672+jweun1@users.noreply.github.com>
2021-03-19 19:28:32 +0900

    Update main.c
    - modify Jiwoo
    - add comments

--- v1/main.c
+++ v1/main.c
@@ -2,11 +2,11 @@
 
 int main(int argc, char *argv[]){
 	
-	int BD;
-	char* toFind;
+	int BD; //BFS or DFS
+	char* toFind; //File or Dir Name to Find
 	char* absDir;
 	
-	if(argc != 3){
+	if(argc != 3){ //Need more data
 		fprintf(stderr, "Usage : Program [-Dir] [-File]\n");
 		exit(1);
 	}
@@ -20,13 +20,14 @@ int main(int argc, char *argv[]){
 		dfs_or_bfs(absDir,BD);		
 
 		//지우
-		toFind = getDirName(argv[2]);
-
-		printDir(argv[2], toFind, BD);
+		char tmp_Path[MAX];
+		strcpy(tmp_Path,argv[2]);
+		char* toFind = extract_Filename(tmp_Path); //입력받은 argv[2]에서 파일 또는 디렉터리 이름만을 추출해 toFind에 넣음
+		before_Search(argv[2], toFind, BD);
 
 	}
 	else
-		printf("Selcect Again\n");
+		printf("Select Error(BFS:0, DFS:1)\n");
 	
 	//익준
 	init(argv[1]); 
